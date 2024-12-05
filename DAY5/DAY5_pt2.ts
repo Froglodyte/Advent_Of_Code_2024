602469fb44d447bab1a2583145aba7c5254a5ab6
Froglodyte <69530598+Froglodyte@users.noreply.github.com>
2024-12-05 23:25:35 +0530

    Update DAY5_pt2.ts

--- DAY5/DAY5_pt2.ts
+++ DAY5/DAY5_pt2.ts
@@ -1,40 +1,6 @@
 let pages = (await Bun.file("./DAY5/pages.txt").text()).split("\n").map(nums => nums.split(",").map(nums => Number(nums)))
-
 let rules = (await Bun.file("./DAY5/rules.txt").text()).split("\n").map(nums => nums.split("|").map(nums => Number(nums)))
 
-/*let pages = `75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47`
-  .split("\n")
-  .map((nums) => nums.split(",").map((nums) => Number(nums)));
-
-let rules = `47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13`
-  .split("\n")
-  .map((nums) => nums.split("|").map((nums) => Number(nums)));*/
-  
 let wrongpages = [];
 
 let sum = 0;
