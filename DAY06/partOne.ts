8f282c96661b23c707078ee530e2ab340b313a56
Froglodyte <69530598+Froglodyte@users.noreply.github.com>
2024-12-06 18:52:05 +0530

    Add files via upload

---
+++ DAY06/partOne.ts
@@ -0,0 +1,72 @@
+let input = await Bun.file("./DAY6/map.txt").text();
+let map = input.split('\r\n').map(grid => grid.split(""));
+
+let pos = { x: 50, y: 45 };
+let dir = '^';
+let visited = new Set();
+
+function pointSum() {
+    while (true) {
+        visited.add(`${pos.x},${pos.y}`);
+        map[pos.y][pos.x] = '.';
+
+        switch (dir) {
+            case '^': {
+                let newY = pos.y - 1;
+
+                while (newY >= 0 && map[newY][pos.x] != '#') {
+                    visited.add(`${pos.x},${newY}`);
+                    newY--;
+                }
+
+                if (newY < 0) return visited.size;
+                pos.y = newY + 1;
+                dir = '>';
+                break;
+            }
+            case '>': {
+                let newX = pos.x + 1;
+
+                while (newX < map[0].length && map[pos.y][newX] != '#') {
+                    visited.add(`${newX},${pos.y}`);
+                    newX++;
+                }
+
+                if (newX >= map[0].length) return visited.size;
+                pos.x = newX - 1;
+                dir = 'v';
+                break;
+            }
+            case 'v': {
+                let newY = pos.y + 1;
+
+                while (newY < map.length && map[newY][pos.x] != '#') {
+                    visited.add(`${pos.x},${newY}`);
+                    newY++;
+                }
+
+                if (newY >= map.length) return visited.size;
+                pos.y = newY - 1;
+                dir = '<';
+                break;
+            }
+            case '<': {
+                let newX = pos.x - 1;
+
+                while (newX >= 0 && map[pos.y][newX] != '#') {
+                    visited.add(`${newX},${pos.y}`);
+                    newX--;
+                }
+
+                if (newX < 0) return visited.size;
+                pos.x = newX + 1;
+                dir = '^';
+                break;
+            }
+        }
+        
+        if (pos.x < 0 || pos.x >= map[0].length || pos.y < 0 || pos.y >= map.length) return visited.size;
+    }
+}
+
+console.log(pointSum());
---
+++ DAY06/partTwo.ts
@@ -0,0 +1,71 @@
+let input = await Bun.file("./DAY06/map.txt").text();
+let map = input.split('\r\n').map(grid => grid.split(""));
+
+let startPos = { x: 50, y: 45 };
+let startDir = '^';
+let loopCount = 0;
+
+for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+        if (map[i][j] != '.') continue;
+
+        map[i][j] = '#';
+        let pos = { ...startPos }; 
+        let dir = startDir;
+        
+        if (validLoop(map, pos, dir)) loopCount++;
+
+        map[i][j] = '.';
+    }
+}
+
+console.log(loopCount);
+
+function validLoop(grid, pos, dir) {
+    let visited = new Set(); 
+
+    while (true) {
+        let state = `${pos.x},${pos.y},${dir}`;
+
+        if (visited.has(state)) {
+            return true; 
+        }
+        visited.add(state);
+
+        switch (dir) {
+            case '^': {
+                let newY = pos.y - 1;
+                while (newY >= 0 && grid[newY][pos.x] != '#') newY--;
+                if (newY < 0) return false;
+                pos.y = newY + 1;
+                dir = '>'; 
+                break;
+            }
+            case '>': {
+                let newX = pos.x + 1;
+                while (newX < grid[0].length && grid[pos.y][newX] != '#') newX++;
+                if (newX >= grid[0].length) return false;
+                pos.x = newX - 1;
+                dir = 'v';
+                break;
+            }
+            case 'v': {
+                let newY = pos.y + 1;
+                while (newY < grid.length && grid[newY][pos.x] != '#') newY++;
+                if (newY >= grid.length) return false;
+                pos.y = newY - 1;
+                dir = '<';
+                break;
+            }
+            case '<': {
+                let newX = pos.x - 1;
+                while (newX >= 0 && grid[pos.y][newX] != '#') newX--;
+                if (newX < 0) return false;
+                pos.x = newX + 1; 
+                dir = '^'; 
+                break;
+            }
+        }
+        if (pos.x == 4 && pos.y == 6 && dir == '^') return true; 
+    }
+}
