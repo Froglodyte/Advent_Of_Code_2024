f72c28cdb16050a118fc3d03f886e75e24856c04
Froglodyte <69530598+Froglodyte@users.noreply.github.com>
2024-12-07 18:57:17 +0530

    Add files via upload

--- DAY07/partOne.ts
+++ DAY07/partOne.ts
@@ -1,46 +1,46 @@
-let eqns = await Bun.file("./DAY07/equations.txt").text();
-let lines = eqns.split("\r\n");
+const eqns = await Bun.file("./DAY07/equations.txt").text();
+const lines = eqns.split("\r\n");
 
-let solns = [];
-let vals = [];
+let solns: number[] = [];
+let vals: number[][] = [];
 let sum = 0;
 
-for(let l of lines){
-    solns.push(Number(l.substring(0, l.indexOf(":"))))
-    vals.push(l.substring(l.indexOf(":") + 2, l.length).split(" ").map(num => Number(num)))
+for (let l of lines) {
+    const [target, numbers] = l.split(": ");
+    solns.push(Number(target));
+    vals.push(numbers.split(" ").map(num => Number(num)));
 }
 
-for(let i = 0; i < solns.length; i++){
-    for(let j of combos(vals[i], solns[i])){
-        console.log(j)
-        if(j == solns[i]){
-            sum += solns[i]
-            break;
-        } 
+for (let i = 0; i < solns.length; i++) {
+    const target = solns[i];
+    const numbers = vals[i];
+    const combinations = combos(numbers, target);
+
+    if (combinations.includes(target)) {
+        sum += target;
     }
 }
 
 function combos(nums: number[], maxVal: number): number[] {
-    const operators = ["+", "*", "||"];
-    const results: number[] = [];
-  
-    function helper(currentVal: number, index: number, currentExp: string): void {
-        if(currentVal > maxVal) return
+    const operators = ["+", "*"];
+    const results: Set<number> = new Set();
+
+    function helper(currentVal: number, index: number): void {
+        if (currentVal > maxVal) return;
         if (index == nums.length - 1) {
-            results.push(currentVal);
+            results.add(currentVal);
             return;
         }
-  
+
         const nextNum = nums[index + 1];
-  
         for (const operator of operators) {
-            if (operator == '+') helper(currentVal + nextNum, index + 1, `${currentExp} + ${nextNum}`);
-            if (operator == '*') helper(currentVal * nextNum, index + 1, `${currentExp} * ${nextNum}`);
+            if (operator == "+") helper(currentVal + nextNum, index + 1);
+            if (operator == "*") helper(currentVal * nextNum, index + 1);
         }
     }
-    helper(nums[0], 0, nums[0].toString());
-  
-    return results;
+
+    helper(nums[0], 0);
+    return Array.from(results);
 }
 
-console.log(sum)
+console.log(sum);
--- DAY07/partTwo.ts
+++ DAY07/partTwo.ts
@@ -1,50 +1,53 @@
-let eqns = await Bun.file("./DAY07/equations.txt").text();
-let lines = eqns.split("\r\n");
+const eqns = await Bun.file("./DAY07/equations.txt").text();
+const lines = eqns.split("\r\n");
 
-let solns = [];
-let vals = [];
+let solns: number[] = [];
+let vals: number[][] = [];
 let sum = 0;
 
 for (let l of lines) {
-    solns.push(Number(l.substring(0, l.indexOf(":"))))
-    vals.push(l.substring(l.indexOf(":") + 2, l.length).split(" ").map(num => Number(num)))
+    solns.push(Number(l.substring(0, l.indexOf(":"))));
+    vals.push(l.substring(l.indexOf(":") + 2).split(" ").map(num => Number(num)));
 }
 
 for (let i = 0; i < solns.length; i++) {
-    for(let j of combos(vals[i], solns[i])){
-        console.log(j)
-        if(j == solns[i]){
-            sum += solns[i]
-            break;
-        } 
+    const target = solns[i];
+    const numbers = vals[i];
+    const combinations = combos(numbers, target);
+
+    if (combinations.has(target)) {
+        sum += target;
     }
 }
 
-function combos(nums: number[], maxVal: number): number[] {
+function combos(nums: number[], maxVal: number): Set<number> {
     const operators = ["+", "*", "||"];
-    const results: number[] = [];
-  
+    const results: Set<number> = new Set();
+
     function helper(currentVal: number, index: number, currentExp: string): void {
-        if(currentVal > maxVal) return
-        if (index == nums.length - 1) {
-            results.push(currentVal);
+        if (currentVal > maxVal) return;
+        if (index === nums.length - 1) {
+            results.add(currentVal);
             return;
         }
-  
+
         const nextNum = nums[index + 1];
-  
         for (const operator of operators) {
-            if (operator == '+') helper(currentVal + nextNum, index + 1, `${currentExp} + ${nextNum}`);
-            else if (operator == '*') helper(currentVal * nextNum, index + 1, `${currentExp} * ${nextNum}`);
-            else if (operator == '||') {
+            if (operator === "+") {
+                helper(currentVal + nextNum, index + 1, `${currentExp} + ${nextNum}`);
+            } else if (operator === "*") {
+                helper(currentVal * nextNum, index + 1, `${currentExp} * ${nextNum}`);
+            } else if (operator === "||") {
                 const concatenatedValue = parseInt(currentVal.toString() + nextNum.toString(), 10);
-                helper(concatenatedValue, index + 1, `${currentExp} || ${nextNum}`);
+                if (concatenatedValue <= maxVal) {
+                    helper(concatenatedValue, index + 1, `${currentExp} || ${nextNum}`);
+                }
             }
         }
     }
+
     helper(nums[0], 0, nums[0].toString());
-  
     return results;
 }
 
-console.log(sum)
+console.log(sum);
