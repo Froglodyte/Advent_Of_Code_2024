d6f5a18318cbb1beac93d71ddfe269aad340cebf
Froglodyte <69530598+Froglodyte@users.noreply.github.com>
2024-12-08 14:18:04 +0530

    Add files via upload

---
+++ DAY08/partOne.ts
@@ -0,0 +1,30 @@
+const grid = (await Bun.file("./DAY08/map.txt").text()).split("\r\n").map(row => row.split(""))
+let antennae: Map<string, [number, number][]> = new Map();
+let antiPoints: Set<string> = new Set();
+
+for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[0].length; j++){
+        if(grid[i][j] == '.') continue;
+        antennae.set(grid[i][j], [...(antennae.get(grid[i][j]) || []), [j, i]]);
+    }
+}
+
+for(let posns of antennae.values()){
+    for(let i = 0; i < posns.length; i++){
+        for(let j = i+1; j < posns.length; j++){
+            const [x1, y1] = posns[i];
+            const[x2, y2] = posns[j]
+            const d1 = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+
+            for(let k = 0; k < grid.length; k++){
+                for(let l = 0; l < grid.length; l++){
+                    const d2 = Math.abs(l - x2) + Math.abs(k - y2)
+                    const d3 = Math.abs(l - x1) + Math.abs(k - y1)
+                    if((d3 == d2*2 || d2 == d3*2) && ((k - y1) * (x2 - x1) - (l - x1) * (y2 - y1) == 0)) antiPoints.add(`${l}, ${k}`)      
+                }
+            }
+        }
+    }
+}
+
+console.log(Array.from(antiPoints).length)
---
+++ DAY08/partTwo.ts
@@ -0,0 +1,27 @@
+const grid = (await Bun.file("./DAY08/map.txt").text()).split("\r\n").map(row => row.split(""))
+let antennae: Map<string, [number, number][]> = new Map();
+let antiPoints: Set<string> = new Set();
+
+for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[0].length; j++){
+        if(grid[i][j] == '.') continue;
+        antennae.set(grid[i][j], [...(antennae.get(grid[i][j]) || []), [j, i]]);
+    }
+}
+
+for(let posns of antennae.values()){
+    for(let i = 0; i < posns.length; i++){
+        for(let j = i+1; j < posns.length; j++){
+            const [x1, y1] = posns[i];
+            const[x2, y2] = posns[j]
+
+            for(let k = 0; k < grid.length; k++){
+                for(let l = 0; l < grid.length; l++){
+                    if((k - y1) * (x2 - x1) - (l - x1) * (y2 - y1) == 0) antiPoints.add(`${l}, ${k}`)      
+                }
+            }
+        }
+    }
+}
+
+console.log(Array.from(antiPoints).length);
